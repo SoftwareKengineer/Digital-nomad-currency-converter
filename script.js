f57d4013452d5ffb5bfd8e8526841304d2d3877a
Software Kengineer <SoftwareKengineer@gmail.com>
2023-03-08 19:16:53 -0500

    added another s
    line 173

--- script.js
+++ script.js
@@ -172,7 +172,7 @@ sendButton.addEventListener("click", ()=> countryInfo(countryName));
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 var popup = L.popup()
